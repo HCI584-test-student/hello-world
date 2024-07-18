83cd82b61ccb5d50ad61f2da6c4f4d95019d7c65
HCI584-test-student <charding.iastate@gmail.com>
2024-07-18 11:54:43 -0500

    Some more change to see how merge overwrit works in github

--- hello_CH.py
+++ hello_CH.py
@@ -1,3 +1,5 @@
 # CH this is my changed file
 print("hello, hello?")
 # change to see how a PR to a fork with a renamed project works 
+
+# Some more change to see how merge overwrit works in github
